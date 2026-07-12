2263694dd544a985963c7b925da1e901bcb3a359
Devon <devonnn37@gmail.com>
2026-07-12 13:35:22 +0200

    Update Supabase client initialization keys

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() async {
   // TODO: Replace placeholders with your actual Supabase URL and Anon Key
   await Supabase.initialize(
     url: 'https://ivzloxwkokirozungdxj.supabase.co',
-    anonKey: 'sb_publishable_zQQYp0_h_n3Tlc2FwanFuA_ApGTqc8X',
+    publishableKey: 'sb_publishable_zQQYp0_h_n3Tlc2FwanFuA_ApGTqc8X',
   );
 
   runApp(const MyApp());
